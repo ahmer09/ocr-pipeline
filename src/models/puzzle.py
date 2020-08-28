f0099bde6abc28c17a06ed012b5c3d14d72b7740
Ahmer Iqbal <ahmeriq09@hotmail.com>
2020-08-28 17:22:23 +0200

    git:chore::extract digit applied

--- src/models/puzzle.py
+++ src/models/puzzle.py
@@ -38,3 +38,42 @@ def find_puzzle(image, debug=False):
         if  len(approx) == 4:
             puzzleCnt = approx
             break
+
+    # if the puzzle contour is empty then our script could not find the outline of sudoku so raise error
+    if puzzleCnt is None:
+        raise Exception(("Could not find Sudoku puzzle outline. Try debugging contour steps and threshhold"))
+
+    # check to see if we are visualizing the outline of the detected Sudoku
+    if debug:
+        # draw the contour of the puzzle on the image and then display it on screen
+        output = image.copy()
+        cv2.drawContours(output, [puzzleCnt], -1, (0, 255, 0), 2)
+        cv2.imshow("Puzzle Outline", output)
+        cv2.waitKey(0)
+
+    # apply a four  point perspective transform t both the original image and grayscale image to obtain a top-down
+    # bird's eye view of the puzzle
+    puzzle = four_point_transform(image, puzzleCnt.reshape(4, 2))
+    warped = four_point_transform(gray, puzzleCnt.reshape(4, 2))
+
+    # check to see if we are visualizing the perspective transform
+    if debug:
+        cv2.imshow("Puzzle Transform", puzzle)
+        cv2.waitKey(0)
+
+    # return 2 tuple of puzzle in both RGB and grayscale
+    return (puzzle, warped)
+
+def extract_digit(cell, debug=False):
+    # apply automatic thresholding to the cell and then clear any
+    # connected borders that touch the border of the cell
+    thresh = cv2.threshold(cell, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    thresh = clear_border(thresh)
+
+    # check to see if we are visualizing the cell thresholding step
+    if debug:
+        cv2.imshow("Cell Thresh", thresh)
+        cv2.waitKey(0)
+
+
+
