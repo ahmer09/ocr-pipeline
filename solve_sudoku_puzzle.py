ad0fe8a1e3ee9309bad368b4f6850c241b487a3f
ahmer iqbal <ahmer.iqbal@foundercore.org>
2020-09-04 23:26:49 +0200

    git::chore : solver config created

--- solve_sudoku_puzzle.py
+++ solve_sudoku_puzzle.py
@@ -36,3 +36,72 @@ stepY = warped.shape[0] // 9
 # initialize a list to store the (x, y)-coordinates of each cell
 # location
 cellLocs = []
+
+# loop over the grid locations
+for y in range(0, 9):
+    # initialize the current list of cell functions
+    row = []
+
+    for x in range(0, 9):
+        # compute the starting and ending (x, y)-coordinates of the
+        # current cell
+        startX = x * stepX
+        startY = y * stepY
+        endX = (x + 1) * stepX
+        endY = (y + 1) * stepY
+
+        # add the (x, y)-coordinates to our cell locations list
+        row.append((startX, startY, endX, endY))
+
+        # crop the cell from the warped transform image and then
+        # extract the digit from the cell
+        cell = warped[startY:endY, startX:endX]
+        digit = extract_digit(cell, debug=args["debug"] > 0)
+
+        # verify that the digit is not empty
+        if digit is not None:
+            # resize the cell to 28x28 pixels and then prepare the
+            # cell for classification
+            roi = cv2.resize(digit, (28, 28))
+            roi = roi.astype("float") / 255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+
+            # classify the digit and update the Sudoku board with the
+            # prediction
+            pred = model.predict(roi).argmax(axis=1)[0]
+            board[y, x] = pred
+
+    # add the row to our cell locations
+    cellLocs.append(row)
+
+# construct a Sudoku puzzle from the board
+print("[INFO] OCR'd Sudoku board:")
+puzzle = Sudoku(3, 3, board=board.tolist())
+puzzle.show()
+
+# solve the Sudoku puzzle
+print("[INFO] solving Sudoku puzzle...")
+solution = puzzle.solve()
+solution.show_full()
+
+# loop over the cell locations and board
+for (cellRow, boardRow) in zip(cellLocs, solution.board):
+    # loop over individual cell in the row
+    for (box, digit) in zip(cellRow, boardRow):
+        # unpack the cell coordinates
+        startX, startY, endX, endY = box
+
+        # compute the coordinates of where the digit will be drawn
+        # on the output puzzle image
+        textX = int((endX - startX) * 0.33)
+        textY = int((endY - startY) * -0.2)
+        textX += startX
+        textY += endY
+
+        # draw the result digit on the Sudoku puzzle image
+        cv2.putText(puzzleImage, str(digit), (textX, textY), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
+
+# show the output image}
+cv2.imshow("Sudoku Result", puzzleImage)
+cv2.waitKey(0)
